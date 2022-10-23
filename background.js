3e803a59553d63c4fa9810b4c0e17b89b327b0c7
Marcos Tomaz <marcoshmpm@gmail.com>
2022-10-23 15:05:57 -0300

    moved script content to src folder

--- background.js
+++
@@ -1,3 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(){
-    window.open('https://steamcommunity.com/tradingcards/boostercreator/','_blank');
-});
---
+++ src/content/constants/config.ts
@@ -0,0 +1,2 @@
+export const steamFee = 0.13043478260869565;
+export const maxRetryCount = 5;
